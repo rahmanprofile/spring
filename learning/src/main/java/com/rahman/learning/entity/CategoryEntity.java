0c0c164392bc54e5336d83c2f5022f23d4270cb9
rAhMaN ~ A DEVIL 💀 <rahman.infodev@gmail.com>
2025-09-07 22:33:06 +0530

    awsdfghjk

--- learning/src/main/java/com/rahman/learning/entity/CategoryEntity.java
+++ learning/src/main/java/com/rahman/learning/entity/CategoryEntity.java
@@ -9,12 +9,12 @@ public class CategoryEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     private String name;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
--- learning/src/main/java/com/rahman/learning/repository/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.rahman.learning.repository;
-import com.rahman.learning.entity.CategoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
-}
